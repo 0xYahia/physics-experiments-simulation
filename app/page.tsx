b5be12f56be05f5fcfaa0220e2f07ad019f853d2
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-09-30 00:34:44 +0300

    add title

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function test(){
 
     return (
     <div className="flex justify-center ">
-        <div className="flex justify-start align-middle flex-row-reverse gap-[4px] w-[1408px] h-[49px] p-2 bg-[#EAEAEA] m-3 rounded-xl">
-            <div className="bg-[#777] w-[25px] h-[25px] mt-1 rounded-full"></div>
+        <div className="flex justify-start align-middle flex-row-reverse gap-[4px] text-white w-full h-[56px] p-2 bg-[#5484FF]">
+            <img src="./assets/image 23.png" alt="" />
             <div>محاكاة التجارب الفيزيائيه</div>
         </div>
     </div>)
