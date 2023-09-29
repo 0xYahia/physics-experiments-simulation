34b906e06c52c09d76b2684693de9d4a70074ac1
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-09-30 01:43:54 +0300

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image';
+
 export default function test(){
 
     return (
     <div className="flex justify-center ">
-        <div className="flex justify-start align-middle flex-row-reverse gap-[4px] text-white w-full h-[56px] p-2 bg-[#5484FF]">
-            <img src="./assets/image 23.png" alt="" />
-            <div>محاكاة التجارب الفيزيائيه</div>
+        <div className="flex justify-start align-middle flex-row-reverse gap-[8px] text-white w-full h-[56px] py-[8px] pr-[24px] bg-[#5484FF]">
+            <Image src='/assets/image1.png' alt="image1" width={40} height={40} />
+            <div className='flex items-center'>محاكاة التجارب الفيزيائيه</div>
         </div>
     </div>)
 
