471de3feb5981cf24c7edcde1a62a87494c4534e
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-09-30 19:40:20 +0300

    plants

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import Link from 'next/link';
 export default function test(){
 
     return (
-    <div >
-        <div className="flex justify-start align-middle gap-[8px] text-white w-full h-[56px] py-[8px] pr-[24px] bg-[#5484FF]">
+    <div className='container px-6' >
+        {/* Header */}
+        <div className="header flex justify-between items-center fixed z-50 top-0 left-0 px-6 text-white w-full h-[56px] py-2 bg-[#5484FF]">
             <Image src='/assets/image1.png' alt="image1" width={40} height={40} />
-            <div className='flex items-center font-bold text-[18px] mt-[5px]'>محاكاة التجارب الفيزيائيه</div>
+        <div className='flex items-center font-bold text-[18px] mt-[5px]'>محاكاة التجارب الفيزيائيه</div>
         </div>
-        <div className='p-6 mt-6'>
+        <div className='p-6 mt-20'>
             <p className='text-[32px] font-bold'>مرحبا بك في برنامج محاكاة التجارب الفيزيائيه</p>
         </div>
         {/* Cards */}
