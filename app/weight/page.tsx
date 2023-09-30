e0d6decc50aecba3cf4eaab902fd5a7023b0b798
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-10-01 00:07:57 +0300

    text cursol

--- app/weight/page.tsx
+++ app/weight/page.tsx
@@ -1,7 +1,29 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
-export default function test(){
+export default function Test(){
+  const [contentIndex, setContentIndex] = useState(0);
+  const contentList = [
+        'في هذه التجربه سوف نتعرف علي المقصود بالوزن (الثقل).',
+        'اسحب احدي الكرات من اليمين واجعلها تسقط سقوطا حرا علي كل من الكواكب المبينه بالشكل, \n مع ملاحظة قيمة تسارع الجاذبيه علي الكواكب المختلفه.',
+        'قم بإيقاف التجربه مستخدما زر الايقاف والتشغيل, ولاحظ وزن كل كره علي الكواكب المختلفه \n خلال قيمة الوزن المصاحب للكره.',
+        'يمكنك حساب وزن كل كره علي اي من الكواكب باستخدام القانون / ق(نيوتن) = ك (كيلو جرام) X ج(م/ث2)',
+        'علي اي كوكب من الكواكب الموجوده يكون وزن الجسم اكبر ؟ \n أ- علي الارض            ب- علي القمر            ج- علي المريخ',
+    ]
+
+    const handleNextClick = () => {
+        if(contentIndex < contentList.length - 1){
+            setContentIndex(contentIndex + 1)
+        }
+    }
+
+    const handlePrevClick = () => {
+        if(contentIndex > 0){
+            setContentIndex(contentIndex - 1)
+        }
+    }
 
     return (
         <div className="container px-6">
@@ -56,7 +78,7 @@ export default function test(){
                     <div className='flex flex-col items-center gap-6'>
                         <hr className='bg-[##ffffff54] w-[110px] h-[1px]'/>
                         <button className='bg-[#FBAC14]  w-[110px] h-[50px] rounded-lg text-[18px] text-white font-black'>تشغيل</button>
-                        <div className='p-2 mt-2 w-[50px] h-[50px] rounded-full bg-[#FBAC14]'>
+                        <div className='p-2 mt-2 w-[50px] h-[50px] rounded-full bg-[#FBAC14] cursor-pointer'>
                             <Image src='/assets/refresh.png' alt="image1" width={32} height={32} />
                         </div>
                     </div>
@@ -78,9 +100,14 @@ export default function test(){
                         </div>
                     </div>
                     <div className="content flex justify-center items-center px-10 py-8 gap-10 border-t-[1px] border-[#CCD2D9]">
-                        <Image src='/assets/right.png' alt="image1" width={40} height={40} />
-                        <p>قم بإيقاف التجربه مستخدما زر الايقاف والتشغيل, ولاحظ وزن كل كره علي الكواكب المختلفه خلال قيمة الوزن المصاحب للكره.</p>
-                        <Image src='/assets/left.png' alt="image1" width={40} height={40} />
+                        {contentIndex !== (contentList.length - 1) ? <Image src='/assets/right.png' alt="image1"width={40} height={40} className='cursor-pointer' onClick={handleNextClick} /> :
+                        <Image src='/assets/lastRight.png' alt="image1" width={40} height={40} className='cursor-pointer' />
+                        }
+                        <p className='text-[22px] font-medium text-[#252C3C] whitespace-pre' >{contentList[contentIndex]}</p>
+                        {contentIndex !== 0 ? <Image src='/assets/left.png' alt="image1" width={40} height={40} className='cursor-pointer' onClick={handlePrevClick} /> :
+                        <Image src='/assets/lastLeft.png' alt="image1" width={40} height={40} className='cursor-pointer' />
+                        }
+
                     </div>
                 </div>
             </div>
